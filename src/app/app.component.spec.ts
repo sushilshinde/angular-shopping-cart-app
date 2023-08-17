f3c659555dea19457fde6b7b2b27383f3176accd
DivyaKanchugatla <divyakanchugatla5@gmail.com>
2023-08-17 19:38:04 +0530

    unit testing is added for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed,ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing'; // Import the provideMockStore function
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoryListComponent } from './components/navbar/category-list/category-list.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component:AppComponent;
+  let fixture: ComponentFixture<AppComponent>;// Define the fixture variable in a wider scope
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule, MatToolbarModule, MatIconModule, MatMenuModule, MatDividerModule, FormsModule],
+      declarations: [AppComponent, HeaderComponent, NavbarComponent, CategoryListComponent],
+      providers: [
+        // Provide the mock store with an initial state
+        provideMockStore({
+          initialState: {} // Add your initial state here
+        }),
+      ],
+    }).compileComponents();;
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-shopping-cart-app app is running!');
+    expect(component).toBeTruthy();
+  });
+
+  
+
+  
+   // Unsubscribe from subscription on ngOnDestroy
+  it('should unsubscribe from subscription on ngOnDestroy', () => {
+    fixture.detectChanges();
+    const subscriptionMock = jasmine.createSpyObj('Subscription', ['unsubscribe']);
+    component.subscription = subscriptionMock;
+
+    component.ngOnDestroy();
+
+    expect(subscriptionMock.unsubscribe).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    fixture.destroy(); // Clean up the component fixture
   });
 });

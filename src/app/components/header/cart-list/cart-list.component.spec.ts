fd5f96dfc7edac6a5d0bfe5d04b600c7dada05c6
DivyaKanchugatla <divyakanchugatla5@gmail.com>
2023-08-18 12:10:18 +0530

    unit testing is added to cart-list and category-list components

--- src/app/components/header/cart-list/cart-list.component.spec.ts
+++ src/app/components/header/cart-list/cart-list.component.spec.ts
@@ -1,21 +1,70 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatIconModule } from '@angular/material/icon';
 import { CartListComponent } from './cart-list.component';
+import { Router } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 describe('CartListComponent', () => {
   let component: CartListComponent;
   let fixture: ComponentFixture<CartListComponent>;
+  let router: Router;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CartListComponent],
+      imports: [RouterTestingModule, MatIconModule,MatListModule],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CartListComponent]
-    });
     fixture = TestBed.createComponent(CartListComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render cart items', () => {
+    const cartData = [
+      { product: { title: 'Product 1', price: 10 }, quantity: 2 },
+      { product: { title: 'Product 2', price: 15 }, quantity: 1 },
+    ];
+    component.cartData = cartData;
+    fixture.detectChanges();
+
+    const cartItems = fixture.nativeElement.querySelectorAll('mat-list');
+    expect(cartItems.length).toEqual(cartData.length);
+
+    cartItems.forEach((item, index) => {
+      const cartItem = cartData[index];
+      expect(item.textContent).toContain(cartItem.product.title.toUpperCase());
+      expect(item.textContent).toContain(cartItem.product.price.toFixed(2));
+      expect(item.textContent).toContain(`x ${cartItem.quantity}`);
+      expect(item.textContent).toContain((cartItem.product.price * cartItem.quantity).toFixed(2));
+    });
+  });
+
+  it('should navigate to cart page on "View Cart" link click', () => {
+    const link = fixture.nativeElement.querySelector('a[routerLink="/cart"]');
+    link.click();
+    fixture.detectChanges(); // Manually trigger change detection
+  
+    const spy = spyOn(router, 'navigateByUrl');
+    expect(spy).toHaveBeenCalledWith('/cart');
+  });
+  
+
+  it('should navigate to checkout page on "Buy Now" button click', () => {
+    const link = fixture.nativeElement.querySelector('button[routerLink="/checkout"]');
+    link.click();
+    fixture.detectChanges();
+    const spy = spyOn(router, 'navigateByUrl');
+    expect(spy).toHaveBeenCalledWith('/checkout');
+  });
+
+  // Add more test cases as needed
 });
--- src/app/components/navbar/category-list/category-list.component.spec.ts
+++ src/app/components/navbar/category-list/category-list.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { CategoryListComponent } from './category-list.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree'; 
+import { Router } from '@angular/router';
 
 describe('CategoryListComponent', () => {
   let component: CategoryListComponent;
   let fixture: ComponentFixture<CategoryListComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CategoryListComponent],
+      imports: [RouterTestingModule, MatIconModule, MatTreeModule], // Import necessary modules
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CategoryListComponent]
-    });
     fixture = TestBed.createComponent(CategoryListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +24,45 @@ describe('CategoryListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle category list on click', () => {
+    const link = fixture.nativeElement.querySelector('a');
+    link.click();
+    fixture.detectChanges();
+    expect(component.isCatOpen).toBe(false);
+
+    link.click();
+    fixture.detectChanges();
+    expect(component.isCatOpen).toBe(true);
+  });
+
+  it('should render category items', () => {
+    const categoryItems = fixture.nativeElement.querySelectorAll('a[routerLink]');
+    console.log('Found category items:', categoryItems);
+  
+    expect(categoryItems.length).toEqual(component.catList.length);
+  
+    categoryItems.forEach((link, index) => {
+      console.log('Checking category item at index', index);
+      console.log('Expected:', component.catList[index]);
+      console.log('Actual:', link.textContent.trim());
+      expect(link.textContent.trim()).toEqual(component.catList[index]);
+    });
+  });
+  
+
+  it('should open category list on "home" route', () => {
+    const router = TestBed.inject(Router);
+    router.navigate(['home']);
+    fixture.detectChanges();
+    expect(component.isCatOpen).toBe(true);
+  });
+
+  it('should close category list on other routes', () => {
+    const router = TestBed.inject(Router);
+    router.navigate(['products', 'shirts']); // Assuming this is a route other than "home"
+    fixture.detectChanges();
+    expect(component.isCatOpen).toBe(false);
   });
 });

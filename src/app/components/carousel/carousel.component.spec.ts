9ae5b4f5c8dfdddc2c1ee3f901158515b4fed989
DivyaKanchugatla <divyakanchugatla5@gmail.com>
2023-08-24 11:19:04 +0530

    changes in carousel spec file

--- src/app/components/carousel/carousel.component.spec.ts
+++ src/app/components/carousel/carousel.component.spec.ts
@@ -10,7 +10,7 @@ describe('CarouselComponent', () => {
  beforeEach(async () => {
 await TestBed.configureTestingModule({
 declarations: [CarouselComponent],
-imports: [NgbCarouselModule], // Import necessary modules
+imports: [NgbCarouselModule], 
 }).compileComponents();
 });
 

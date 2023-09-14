ed30ff380c30961734659622babb09cf7e26c876
Kanchugatla Divya <dkanchugatla@AD.MSYSTECHNOLOGIES.COM>
2023-09-14 13:15:09 +0530

    changes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -31,8 +30,7 @@ import { ShopModule } from './modules/shop/shop.module';
     CategoryListComponent,
     CartListComponent,
     NotFoundComponent,
-    ProductSearchComponent,
-   
+    ProductSearchComponent, 
   ],
   imports: [
     BrowserModule,
